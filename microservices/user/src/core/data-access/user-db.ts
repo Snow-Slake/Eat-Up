83cb1580a1033f8d82c2a3702ee8a94eb036cc35
ahmedShakshak <ahmedshakshak.fcis@gmail.com>
2021-01-07 02:29:01 +0200

    implemented update user

--- microservices/user/src/core/data-access/user-db.ts
+++ microservices/user/src/core/data-access/user-db.ts
@@ -8,8 +8,12 @@ export default class makeUserDb implements UserDb {
         let userKey = (await db.ref(DATABASE.USER_COLLECTION_ENTRY).push(user.toJson())).key;
         return userKey != null;
     }
-    update(user: User): Promise<boolean> {
-        throw new Error("Method not implemented.");
+
+    async update(user: User): Promise<boolean> {
+        let update = {};
+        update["/" + DATABASE.USER_COLLECTION_ENTRY + "/" + user.id] = user.toJson();
+        let ref = await db.ref().update(update);
+        return ref != null;
     }
     delete(user: User): Promise<boolean> {
         throw new Error("Method not implemented.");
