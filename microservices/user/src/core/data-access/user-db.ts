02bf95c31a9bad0b71ed210bc0e7c371b383f16a
ahmedShakshak <ahmedshakshak.fcis@gmail.com>
2021-01-07 02:27:18 +0200

    implemented insert user function

--- microservices/user/src/core/data-access/user-db.ts
+++ microservices/user/src/core/data-access/user-db.ts
@@ -1,11 +1,12 @@
 import { User } from "../entities/user";
 import { UserDb } from "../usecases";
+import { DATABASE } from "../../config";
+import { db, userDb } from "./index";
 
 export default class makeUserDb implements UserDb {
-    constructor (private makeDb: Function) {}
-
-    insert(user: User): Promise<boolean> {
-        throw new Error("Method not implemented.");
+    async insert(user: User): Promise<boolean> {
+        let userKey = (await db.ref(DATABASE.USER_COLLECTION_ENTRY).push(user.toJson())).key;
+        return userKey != null;
     }
     update(user: User): Promise<boolean> {
         throw new Error("Method not implemented.");
