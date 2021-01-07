187fd2a0c33d1c71678a046472d39161ab4d5b5c
ahmedShakshak <ahmedshakshak.fcis@gmail.com>
2021-01-07 02:31:59 +0200

    made db = firebase.database() instead of firebase name space

--- microservices/user/src/core/data-access/admin.ts
+++ microservices/user/src/core/data-access/admin.ts
@@ -1,7 +1,7 @@
-import firebase from 'firebase';
-import 'firebase/firestore';
-import { firebaseConfig } from '../../config/db-connection';
+import firebase from "firebase";
+import "firebase/firestore";
+import { firebaseConfig } from "../../config/db-connection";
 
 firebase.initializeApp(firebaseConfig);
 
-export const db = firebase;
+export const db = firebase.database();
