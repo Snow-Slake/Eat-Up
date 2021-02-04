16b29fc9e182922e34885ed966ba1cc6afc88b27
Ahmed Amin <ahmed@192.168.1.7>
2021-02-04 19:06:43 +0200

    added post architecture

---
+++ microservices/post/src/config/db-connection.ts
@@ -0,0 +1,17 @@
+export const serviceAccountKey = require("./serviceAccountKey.json");
+export const bucketName = "eat-up-e24f9.appspot.com";
+export const bucketUrl = "gs://eat-up-e24f9.appspot.com/";
+export const read = "read";
+export const expiration = "03-09-10000";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyC9rTku3KKUzTkgjjciJQccJbGTazSdr3I",
+    authDomain: "eat-up-e24f9.firebaseapp.com",
+    projectId: "eat-up-e24f9",
+    storageBucket: "eat-up-e24f9.appspot.com",
+    messagingSenderId: "384089370264",
+    appId: "1:384089370264:web:71d8c6340ae794d970eaed",
+    measurementId: "G-D9NSW1M0L1",
+    serviceAccount: serviceAccountKey,
+};
---
+++ microservices/post/src/config/index.ts
@@ -0,0 +1,1 @@
+export * as DB_CONFIG from "./db-connection";
